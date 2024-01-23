32192f020fca9b9bc61438b00a10d4b1e4317919
Edinson <edisonprise@hotmail.com>
2024-01-22 21:56:36 -0500

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/edisonprise/tutorialMorsaSpring/services/OrderService.java
+++ src/main/java/com/edisonprise/tutorialMorsaSpring/services/OrderService.java
@@ -4,15 +4,19 @@ import com.edisonprise.tutorialMorsaSpring.Rutas;
 import com.edisonprise.tutorialMorsaSpring.models.Producto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public class OrderService implements IOrderService {
 
+    @Value("${misurls.database}")
+    private String databaseurl;
+
     private final Logger logger = LoggerFactory.getLogger(OrderService.class);
     public void saveOrder(List<Producto> productos) {
-        System.out.println("Guardando en la base de datos...");
+        System.out.println("Guardando en la base de datos, la url: " + databaseurl);
         productos.forEach(producto -> logger.debug("el nombre del producto: {}", producto.nombre));
 
     }
